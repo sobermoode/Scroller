464892cc21ed5cec0826ad9be3ba85a13b5fd6b4
Aaron <J>
2016-04-16 18:53:48 -0700

    Created the GameOverScene class, but I forgot to merge into Master, first.

---
+++ Scroller/GameOverScene.swift
@@ -0,0 +1,13 @@
+//
+//  GameOverScene.swift
+//  Scroller
+//
+//  Created by Aaron Justman on 4/16/16.
+//  Copyright © 2016 AaronJ. All rights reserved.
+//
+
+import SpriteKit
+
+class GameOverScene: SKScene {
+
+}
